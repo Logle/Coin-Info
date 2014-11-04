7038e17da2d2800908259a61e53551a744939198
Logle <vqlong2014@gmail.com>
2014-11-04 12:44:57 -0500

    second commit
    remove jade, install swig, cheerio

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var swig = require('swig');
+app.engine('html', swig.renderFile);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -37,6 +39,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+    swig.setDefaults({cache: false});
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
