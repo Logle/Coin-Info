e20cde4db7b625c4c18055cf584045a8fa3ed3cf
Logle <vqlong2014@gmail.com>
2014-12-18 20:37:49 -0500

    re-design front end
    fix some front end errors

--- public/javascripts/utility.js
+++ public/javascripts/utility.js
@@ -1,22 +1,8 @@
 $(document).ready(function(){
-	
+
 	$.get('/bitcoinprice', function(price){
-		
-		if (price.last > price.low){
-			$('#pricetick-div').append("<div><strong>last price " + price.last.toString()+ " usd </strong><span id='burning' class='glyphicon glyphicon-fire'></span></div>");
-		} else {
-			$('#pricetick-div').append("<div><strong>last price " + price.last.toString()+ " usd </strong><span class='glyphicon glyphicon-cloud'></span></div>");
-		};
-		// var burningTime = 0;
-		// var tickerInterval = window.setInterval(function(){
-		// 	 if (burningTime <20) { 
-		// 	 	  $('#burning').toggleClass('glyphicon glyphicon-fire');  burningTime++; 
-		// 	 } else {
-		// 	 		window.clearInterval(tickerInterval);
-		// 	 }
-		// }, 500);	
-	
-	})
+		$('#pricetick-div').append("<div><strong>Bitcoin last price " + price.last.toString()+ " usd </strong></div>");
+	});
 
 	$('#searchBox').typeahead({
 		hint: true,
@@ -26,9 +12,9 @@ $(document).ready(function(){
 	{
 		name: 'article-data',
   	displayKey: 'title',
-  	source: function(query, callback){	
+  	source: function(query, callback){
   		return $.get('/searchBox', {query:query}, function(data){
-				callback(data);	
+				callback(data);
   		});
   	}
 	});
