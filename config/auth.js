d82edeab960d42163680cdf16b4ac4a8873e83bc
Logle <vqlong2014@gmail.com>
2014-12-23 14:40:45 -0500

    add developer id back

--- config/auth.js
+++ config/auth.js
@@ -8,8 +8,8 @@ var facebookSecret = process.env.FACEBOOK_SECRET;
 
 module.exports = {
     'facebookAuth' : {
-        'clientID'      :  // your App ID
-        'clientSecret'  :  // your App Secret
+        'clientID'      :  1398448437115065, // your App ID
+        'clientSecret'  :  '2a8f375ef85f7713c095cf4385584d69',// your App Secret
         'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
     },
 
